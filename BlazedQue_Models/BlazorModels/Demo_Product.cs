3c4faee40e485b791571d0a4c4bd25f72be55ef6
TOMMY-QURESHI <qureshitricky@gmail.com>
2022-05-25 16:26:22 +0500

    added Class Library and Demo Product

---
+++ BlazedQue_Models/BlazorModels/Demo_Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazedQue_Models.BlazorModels
+{
+    internal class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public int Price { get; set; }
+
+
+    }
+}
